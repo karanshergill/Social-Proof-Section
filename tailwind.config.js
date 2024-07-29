32b62428da9e5e024f1a29d6b069fa78c94c4513
Karan <email@karanshergill.com>
2024-07-29 13:01:31 +0530

    project main files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'Primary-Very-Dark-Magenta': 'hsl(300, 43%, 22%)',
+        'Primary-Soft-Pink' : 'hsl(333, 80%, 67%)',
+        'Neutral-Dark-Grayish-Magenta': 'hsl(303, 10%, 53%)',
+        'Neutral-Light-Grayish-Magenta': 'hsl(300, 24%, 96%)',
+        'Neutral-White': 'hsl(0, 0%, 100%)'
+      },
+      fontFamily: {
+        'League-Spartan': ["League Spartan", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
