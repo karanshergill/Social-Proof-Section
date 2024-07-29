0df0ec00fe48028d0aa2412fd7e3cc0dbd2d6a0d
Karan <email@karanshergill.com>
2024-07-29 18:04:57 +0530

    fix render

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const starContainers = document.querySelectorAll('.stars');
-    const starImage = '<img src="/images/icon-star.svg" class="w-4 h-4" alt="Star">';
-    starContainers.forEach(container => {
-        container.innerHTML = starImage.repeat(5);
-    });
-});
